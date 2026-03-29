96c169e90b54570dfe42f671980547cce298531c
ethervs <ethervs@gmail.com>
2026-03-28 23:14:29 -0300

    refactor paths

--- dot-files/init.zsh
+++ dot-files/init.zsh
@@ -1,10 +1,10 @@
 
 # Dot files zsh
-for file in ~/dot-files/zsh/*.zsh; do
+for file in ~/git/dot-files/zsh/*.zsh; do
   source "$file"
 done
 
 # Functions
-for file in ~/dot-files/scripts/*.sh; do
+for file in ~/git/dot-files/scripts/*.sh; do
   source "$file"
 done
